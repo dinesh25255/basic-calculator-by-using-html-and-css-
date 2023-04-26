ebb0f7eb789d9849ebd5041d1d2079212606a178
dinesh25255 <dineshbhatt0707@gmail.com>
2023-04-26 16:39:59 +0545

    shooping list

---
+++ index.js
@@ -0,0 +1,50 @@
+// const addition = (num1, num2 ) =>{
+//     let sum
+//     sum = num1 + num2
+//      console.log(sum)
+//     const subtraction = (num1, num2) => {
+//         let sub
+//         sub = num1 - num2
+//         console.log(sub)
+//         return sub
+//      }
+//     return {sum ,subtraction}
+// }
+
+// const result = addition(1,2).sum
+// const subtraction = addition(1,2).subtraction
+// console.log(result)
+// // addition("the name is", subtraction(2,1))
+
+// // callback function is a function which takes another function as an arguement
+// subtraction(2,1)
+// // const call_back = (arg) => {
+// //     console.log(`THIS IS CALL BACK function ${arg}`)
+// // }
+
+// // call_back(addition(1,2))
+
+// const rest = (num1, num2, ...extra ) =>{
+//     let sum
+//     sum = num1 + num2 + extra[2]
+//      console.log(sum)
+//      console.log(extra)
+//     return sum
+// }
+
+// rest(1,2,3,4,5,6)
+
+let todos = ["css", "html"]
+
+
+// for(let index = 0; index < todos.length; index++){
+//     console.log(todos[index])
+// }
+
+// todos.forEach(element => {
+//     console.log(element)
+// })
+
+
+
+
